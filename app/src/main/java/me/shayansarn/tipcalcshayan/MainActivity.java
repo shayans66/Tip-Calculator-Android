ae3beabf6e6e6965b7250037fdd87bdf0d6ca3a6
shayans66 <42shayan42@gmail.com>
2019-06-04 19:58:15 -0700

    Init

---
+++ app/src/main/java/me/shayansarn/tipcalcshayan/MainActivity.java
@@ -0,0 +1,20 @@
+package me.shayansarn.tipcalcshayan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tipTotalTextView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
