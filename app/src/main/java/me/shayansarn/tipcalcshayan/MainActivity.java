1308f2ebbda428bb85a752d82fd343ff7744907d
shayans66 <42shayan42@gmail.com>
2019-06-04 20:45:08 -0700

    yeet

--- app/src/main/java/me/shayansarn/tipcalcshayan/MainActivity.java
+++ app/src/main/java/me/shayansarn/tipcalcshayan/MainActivity.java
@@ -2,12 +2,20 @@ package me.shayansarn.tipcalcshayan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tipTotalTextView;
+    TextView totalTextView;
+    EditText billTotalEditText;
 
+    Button tenPercentButton;
+    Button fifteenPercentButton;
+    Button twentyPercentButton;
 
 
     @Override
@@ -15,6 +23,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tipTotalTextView = findViewById(R.id.tipTotalTextView);
+        //tipTotalTextView.setText("Why hello there");
+        totalTextView = findViewById(R.id.totalTextView);
+        billTotalEditText = findViewById(R.id.billTotalEditText);
+
+        tenPercentButton = findViewById(R.id.tenButton);
+        fifteenPercentButton = findViewById(R.id.twentyButton);
+        twentyPercentButton = findViewById(R.id.thirtyButton);
+
+        tenPercentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Determine the Tip
+                    //double tip = 0.10 * billTotalEditText.get
+
+                Double billTotal = Double.parseDouble(billTotalEditText.getText().toString() );
+
+
+                // Display tip
+
+                // Determine total
+
+                // Display total
+            }
+        });
 
     }
 }
